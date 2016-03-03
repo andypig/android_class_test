fd75b87def6ccd78a60562a3d49a9acd0d743b84
Andypig <a760405@gmail.com>
2016-03-03 22:00:20 +0800

    Android Test Project
    FUCK

---
+++ Android_Class_With_Git_Test/app/src/main/java/com/example/diva/android_class_with_git_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.diva.android_class_with_git_test;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
