609c94d9b2caaffcc275d93999dab34e48fac41e
Andypig <a760405@gmail.com>
2016-03-07 20:16:36 +0800

    新增按鈕按下去的是反應事件Submit
    當按下按鈕後會跳出"Hello DUDE!!"的訊息框

--- SimpleUI/app/src/main/java/com/example/diva/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/diva/simpleui/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.diva.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submit(View view)
+    {
+        Toast.makeText(this, "Hello DUDE!!!", Toast.LENGTH_LONG).show();// 第一個參數把自己傳入，第二個是要顯示的東西，第三個是顯示的時間
+    }
+
 }
