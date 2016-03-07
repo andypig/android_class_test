3b8bd02433d561dcf93ffa39a24566233e974906
Andypig <a760405@gmail.com>
2016-03-07 21:55:43 +0800

    輸入的PlainText設定
    PlainText設定與按鈕事件的設定

--- SimpleUI/app/src/main/java/com/example/diva/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/diva/simpleui/MainActivity.java
@@ -2,20 +2,86 @@ package com.example.diva.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    CheckBox hidecheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+        hidecheckBox = (CheckBox)findViewById(R.id.checkBox);
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) //前面定義按下去的按鈕為enter，後面定義時機
+                {
+                    submit(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() { // 設定可以偵測虛擬鍵盤的輸入
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_DONE)
+                {
+                    submit(v);
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     public void submit(View view)
     {
-        Toast.makeText(this, "Hello DUDE!!!", Toast.LENGTH_LONG).show();// 第一個參數把自己傳入，第二個是要顯示的東西，第三個是顯示的時間
+        //Toast.makeText(this, "Hello DUDE!!!", Toast.LENGTH_LONG).show();// 第一個參數把自己傳入，第二個是要顯示的東西，第三個是顯示的時間
+
+        //textView.setText("Test Test Test!!!");
+
+        String text = editText.getText().toString();
+
+        if(hidecheckBox.isChecked())
+        {
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+
+            textView.setText("你到底輸入了甚麼??");
+            editText.setText("不要問，很恐怖");
+
+            //間接寫法，如此就不會再繼續往下執行，所以下面的else也不用加了!!!
+            return;
+        }
+        //else {
+        //    textView.setText(text);
+        //    editText.setText("");
+        //}
+
+        //textView.setText(text);
+        //editText.setText(""); //清除原本的輸入，也就是設為空字串
+
+        // 把輸入的值隱藏起來，全部用*取代
+        //textView.setText(text);
+        //editText.setText("");
+
+        textView.setText(text);
+        editText.setText("");
     }
 
 }
