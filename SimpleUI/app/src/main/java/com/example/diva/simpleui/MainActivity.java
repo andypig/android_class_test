8871b1355f9320c4845b2b3615c025efb800d9d3
Andypig <a760405@gmail.com>
2016-03-10 20:01:31 +0800

    新增SharedPreference的兩個變數來kep使用者輸入的內容

--- SimpleUI/app/src/main/java/com/example/diva/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/diva/simpleui/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.diva.simpleui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editText;
     CheckBox hidecheckBox;
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +29,31 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.editText);
         hidecheckBox = (CheckBox)findViewById(R.id.checkBox);
 
+        sp = getSharedPreferences("setting", Context.MODE_PRIVATE); // 定義setting裡面的東西，供之後使用
+        editor = sp.edit();
+
+        editText.setText(sp.getString("editText", "")); // 取出sp裡面editText的內容，存回到editText
+
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) //前面定義按下去的按鈕為enter，後面定義時機
+                editor.putString("editText", editText.getText().toString());//把所key入的內容寫入到editText裡面，然後存起來
+                editor.apply();
+
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) //前面定義按下去的按鈕為enter，後面定義時機
                 {
                     submit(v);
                     return true;
                 }
                 return false;
             }
+
         });
 
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() { // 設定可以偵測虛擬鍵盤的輸入
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_DONE)
-                {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
                     submit(v);
                     return true;
                 }
@@ -48,6 +61,18 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        hidecheckBox.setChecked(sp.getBoolean("hideCheckbox", false));
+
+        hidecheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean("hideCheckbox", hidecheckBox.isChecked());
+                editor.apply();
+            }
+        });
+
+
     }
 
     public void submit(View view)
