3864de98e31afe8639122628fb59fafe6a72980f
Andypig <a760405@gmail.com>
2016-03-30 09:30:32 +0800

    抓到照片經緯度後使用google map顯示地圖

--- SimpleUI/app/src/main/java/com/example/diva/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/diva/simpleui/OrderDetailActivity.java
@@ -1,16 +1,17 @@
 package com.example.diva.simpleui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -18,6 +19,12 @@ public class OrderDetailActivity extends AppCompatActivity {
     TextView storeInfo;
     TextView menu;
     ImageView photo;
+    ImageView staticMapImageView;
+    WebView webView;
+
+    // 怕url在運行時會消失，所以用在class下得變數去接
+    //private String url;
+    //private String address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +35,11 @@ public class OrderDetailActivity extends AppCompatActivity {
         storeInfo = (TextView) findViewById(R.id.storeInfoView);
         menu = (TextView) findViewById(R.id.menuView);
         photo = (ImageView) findViewById(R.id.photoView);
+        staticMapImageView = (ImageView) findViewById(R.id.staticMapImageView);
+        webView = (WebView) findViewById(R.id.webView);
 
         note.setText(getIntent().getStringExtra("note"));
+        String storeInformation= getIntent().getStringExtra("storeInfo");
         storeInfo.setText(getIntent().getStringExtra("storeInfo"));
         String menuResult = getIntent().getStringExtra("menu");
 
@@ -52,8 +62,111 @@ public class OrderDetailActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+
+        String address = storeInformation.split(",")[1];
+
+        new GeoCodingTask().execute(address);
+
         String url = getIntent().getStringExtra("photoURL");
-        if(url != null)
-        Picasso.with(this).load(url).into(photo);
+
+        // 1. 原本寫法
+        //上方的改寫，上面是簡單版，下面是抓url後去做拿圖片
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+////                String url = Utils.getGeoCodingURL(address);
+////                byte[] bytes = Utils.urlToBytes(url);
+////                String result = new String(bytes);
+////
+//                double[] locations = Utils.getLatLngFromJsonString(address);
+//                String debugLog = "lat" + String.valueOf(locations[0]) + "lng" + String.valueOf(locations[1]);
+//                Log.d("debug", "debugLog :" + debugLog);
+//
+//                // 用thread跑load string不好
+//                //Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//                //photo.setImageBitmap(bmp);
+//            }
+//        });
+//        thread.start();
+        //url = this.getIntent().getStringExtra("photoURL");
+        if(url==null){
+            return;
+        }
+
+        //url = getIntent().getStringExtra("photoURL");
+
+        // 避免如果該筆紀錄沒有上傳圖片的話會當掉的狀況
+        // 如果url沒有東西(表示沒有照片)，就直接return，不跑下面的顯示照片
+        new ImageLoadingTask(photo).execute(url);
+
+
+//        // 0. 偷工減料的方法，使用Picasso的資源來抓url並顯示照片
+//        if(url != null)
+//        Picasso.with(this).load(url).into(photo);
+//
+
+
+
+        // 2.改寫更好的方法:
+        //用syncronyze thread 跑比較好，用好幾個thread包起來，並且用物件去拿url，跟一般的thread就差別在他有好幾個thread
+//        new AsyncTask<String, Void, byte[]>() {
+//            @Override
+//            protected byte[] doInBackground(String... params) {
+//                String url = params[0];
+//                return Utils.urlToBytes(url);
+//            }
+//
+//            @Override
+//            protected void onPostExecute(byte[] bytes) {
+//                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//                photo.setImageBitmap(bmp);
+//                super.onPostExecute(bytes);
+//            }
+//        }.execute(url);
+    }
+
+    class GeoCodingTask extends AsyncTask<String, Void, byte[]> {
+        private double[] latlng;
+        private String url;
+
+        @Override
+        protected byte[] doInBackground(String... params){
+            String address = params[0];
+            latlng = Utils.addressToLatLng(address);
+            url = Utils.getStaticMapUrl(latlng, 17);
+            return Utils.urlToBytes(url);
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes){
+            webView.loadUrl(url);
+            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            staticMapImageView.setImageBitmap(bmp);
+            super.onPostExecute(bytes);
+        }
+    }
+
+
+
+    class ImageLoadingTask extends AsyncTask<String, Void, byte[]> {
+        ImageView imageView;
+
+        public ImageLoadingTask(ImageView imageView){
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected byte[] doInBackground(String... params){
+            String url = params[0];
+            return Utils.urlToBytes(url);
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes){
+            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            imageView.setImageBitmap(bmp);
+            super.onPostExecute(bytes);
+        }
     }
 }
+
--- SimpleUI/app/src/main/java/com/example/diva/simpleui/Utils.java
+++ SimpleUI/app/src/main/java/com/example/diva/simpleui/Utils.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Environment;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,6 +14,11 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 /**
  * Created by DIVA on 2016/3/10.
@@ -53,10 +61,10 @@ public class Utils {
 //        return Uri.fromFile(file);
 //    }
 
-    public static Uri getPhotoUri(){
+    public static Uri getPhotoUri() {
         File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
-        if(dir.exists() == false){
+        if (dir.exists() == false) {
             dir.mkdir();
         }
 
@@ -74,12 +82,87 @@ public class Utils {
                 base.write(buffer, 0, len);
             }
             return base.toByteArray();
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static byte[] urlToBytes(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            InputStream is = connection.getInputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+            return baos.toByteArray();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getGeoCodingURL(String address){
+        try {
+            address = URLEncoder.encode(address, "utf-8"); // 先把url轉成utf-8格式，在串上API
+        } catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+
+        String url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + address;
+        return url;
+    }
+
+
+    public static double[] getLatLngFromJsonString(String jsonString){
+        try {
+            JSONObject object = new JSONObject(jsonString);
+
+//            if (!object.getString("status").equals("OK")){
+//                return null;
+
+                JSONObject location = object.getJSONArray("results").getJSONObject(0).getJSONObject("geometry").getJSONObject("location");
+
+                double lat = location.getDouble("lat");
+                double lng = location.getDouble("lng");
+
+                return new double[]{lat, lng};
+
+            } catch (JSONException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // 把前面的Thread thread = new Thread(new Runnable()裡面的run拉過來這邊寫成function去call他
+    public static double[] addressToLatLng(String address) {
+        String url = Utils.getGeoCodingURL(address);
+        byte[] bytes = Utils.urlToBytes(url);
+        String result = new String(bytes);
+
+        double[] locations = Utils.getLatLngFromJsonString(result);
+        //String debugLog = "lat" + String.valueOf(locations[0]) + "lng" + String.valueOf(locations[1]);
+        //Log.d("debug", result); //亂碼，因把url轉成string
+        //Log.d("debug", "debugLog :" + debugLog);
+
+        return locations;
+    }
+
+    public static String getStaticMapUrl(double[] latlng, int zoom){
+        String center = latlng[0] + "," + latlng[1];
+        String url = "http://maps.googleapis.com/maps/api/staticmap?center=" + center + "&zoom=" + zoom + "&size=640x400";
+        return url;
     }
 }
 
